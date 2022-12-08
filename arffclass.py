c3510aa411b0c8a820e892bcaf77ca8cdd8d997b
albertolicea00 <albertolicea00@icloud.com>
2022-12-08 10:30:00 -0500

    Enhance documentation and comments in arffGenerator and example scripts for clarity and bilingual support

--- arffclass.py
+++ arffclass.py
@@ -1,16 +1,31 @@
+"""
+arffGenerator — build a Weka .arff dataset file filled with random data.
+arffGenerator — genera un archivo de dataset .arff de Weka con datos aleatorios.
+
+EN: Define a relation name, a set of attributes (each with its list of
+    possible values) and the class values, then call randomize() to write
+    the .arff file with randomly picked rows.
+ES: Define el nombre de la relación, un conjunto de atributos (cada uno con
+    su lista de valores posibles) y los valores de clase; luego llama a
+    randomize() para escribir el archivo .arff con filas elegidas al azar.
+"""
+
 import random, os
 
 
 class arffGenerator():
+	"""Generator of Weka .arff files with randomized data."""
 
 	def __init__(self):
-		self.__datas = 5 					# cantidad de datos que genera
-		self.__relation = "relacionName"	# nombre de la relacion 
-		self.__fileName = "example.arff"	# nombre del archivo.arff
-		self.__attributes = []				# atributos y sus correspondientes valores
-		self.__class = []					# valores del atributo clasificacion
+		"""Initialize the generator with default values"""
+		self.__datas = 5 					# EN: number of data rows to generate 	/ ES: cantidad de filas a generar
+		self.__relation = "relacionName"	# EN: relation name 					/ ES: nombre de la relacion
+		self.__fileName = "example.arff"	 # EN: output .arff file name 			  / ES: nombre del archivo .arff de salida
+		self.__attributes = []				# EN: attributes and their values 		/ ES: atributos y sus valores
+		self.__class = []					# EN: classification attribute values 	 / ES: valores del atributo de clasificacion
 
 	def __searchAtribute(self, atribute):
+		""" Return the index of an attribute by name; raise if not found."""
 		for i in range(len(self.__attributes)):
 			if self.__attributes[i] == atribute:
 				return i
@@ -18,6 +33,15 @@ class arffGenerator():
 
 
 	def randomize(self):
+		"""
+		EN: Generate the .arff file: writes the @relation header, the
+		    @attribute declarations and @datas rows of random values.
+		    Overwrites the file if it already exists.
+		
+		ES: Genera el archivo .arff: escribe la cabecera @relation, las
+		    declaraciones @attribute y @datas filas de valores aleatorios.
+		    Sobrescribe el archivo si ya existe.
+		"""
 		self.setAttribute("class", self.__class)
 
 		if os.path.exists(self.__fileName):
@@ -25,23 +49,30 @@ class arffGenerator():
 
 		try:
 			with open (file=self.__fileName, mode="w+") as file:
+				# EN: @relation header / ES: cabecera @relation
 				file.write(f"@relation {self.__relation}\n\n")
-				
+
+				# EN: one @attribute line per attribute, values inside { } 
+				# ES: una línea @attribute por atributo, valores entre { }
 				for i in range(len(self.__attributes)):
 					file.write(f"@attribute {self.__attributes[i][0]} " + "{")
 					for j in range(len(self.__attributes[i][1])):
 						file.write(f"{str(self.__attributes[i][1][j])}, ")
 					file.seek(file.tell()-2)
-					file.write("}\n")	
+					file.write("}\n")
 
+				# EN: @data section, one random row per iteration
+				# ES: sección @data, una fila aleatoria por iteración
 				file.write(f"\n@data\n")
 				for z in range(self.__datas):
 					for i in range(len(self.__attributes)):
 						rand = random.randint(0, len(self.__attributes[i][1])-1)
 						value = self.__attributes[i][1][rand]
+						# EN: quote numbers so Weka treats them as nominal
+						# ES: entrecomilla números para que Weka los trate como nominales
 						if type(value) == int or type(value) == float:
 							value = "'" + str(value) + "'"
-						
+
 						file.write(f"{value}, ")
 					file.seek(file.tell()-2)
 					file.write("\n")
@@ -50,62 +81,79 @@ class arffGenerator():
 
 
 	# getters
-	def getDatas():
+	def getDatas(self):
+		"""Return the configured row count."""
 		self.__datas
-	def getRelation():
+	def getRelation(self):
+		"""Return the relation name."""
 		self.__relation
-	def getFileName():
+	def getFileName(self):
+		"""Return the output file name."""
 		self.__fileName
-	def getClass():
+	def getClass(self):
+		"""Return the attributes list."""
 		self.__attributes
-	def getAttribute():
+	def getAttribute(self):
+		"""Return the class values."""
 		self.__class
 
 	# setters
-	def setDatas (self, value):
+	def setDatas(self, value):
+		"""Set the number of data rows to generate."""
 		self.__validateDatas(value)
 		self.__datas = value
 
-	def setRelation (self, value):
+	def setRelation(self, value):
+		"""Set the relation name (@relation)."""
 		self.__validateRelation(value)
 		self.__relation = value
 
-	def setFileName (self, value):
+	def setFileName(self, value):
+		"""Set the output file name; ".arff" is appended automatically."""
 		self.__validateFileName(value)
 		self.__fileName = value +".arff"
 
-	def setClass (self, values):
+	def setClass(self, values):
 		"""
-			>> file.setClass( [valor1, valor2, valor3, ....] )	
+		Set the possible values of the class (classification) attribute.
+
+			>> file.setClass( [valor1, valor2, valor3, ....] )
 		"""
 		self.__validateClass(values)
 		self.__class = values
 
 	def setAttribute(self, atributte, values=[]):
 		"""
-			>> file.setAttribute( atributo, [valor1, valor2, valor3, ....] )	
+		Add an attribute and its list of possible values.
+
+			>> file.setAttribute( atributo, [valor1, valor2, valor3, ....] )
 		"""
 		self.__validateAttribute(atributte, values)
 		self.__attributes += [[atributte, values]]
 
 
-	# validaciones
+	# validaciones / validations
 	def __validateDatas(self, value):
+		"""Require an int"""
 		if type(value) != int:
 			raise Exception("The Datas value must be a integer")
 
 	def __validateRelation(self, value):
+		"""Require a str"""
 		if type(value) != str:
 			raise Exception("The Relation value must be a string")
 
 	def __validateFileName(self, value):
+		"""Require a str"""
 		if type(value) != str:
 			raise Exception("The File name value must be a string")
 
 	def __validateClass(self, values):
+		"""EN: Validate class values as an attribute named "class". / ES: Valida los valores de clase como un atributo llamado "class"."""
 		self.__validateAttribute("class", values)
 
 	def __validateAttribute(self, atribute, values):
+		"""Require attribute name to be a str and values to be a list"""
 		if type(atribute) != str:
 			raise Exception("The atribute value must be a string")
 		elif type(values) != list:
--- simpleconsole.py
+++ simpleconsole.py
@@ -1,13 +1,21 @@
+"""
+EN: Standalone script version (no class). Same logic as arffGenerator.randomize()
+    but configured via plain variables at the top — edit them, then run.
+ES: Versión script standalone (sin clase). Misma lógica que arffGenerator.randomize()
+    pero configurada con variables al inicio — edítalas y ejecuta.
+"""
+
 import random
 
 
 
-datas = 10000000					# cantidad de datos que genera
-relacion = "meteoritos"				# nombre de la relacion y del archivo
+datas = 10000000					# EN: number of data rows to generate 	/ ES: cantidad de datos que genera
+relacion = "meteoritos"				# EN: relation name (also the file name) / ES: nombre de la relacion y del archivo
 
 
 
-# valores a tomar de cada atributo 	[atributo, [valor1, valor2, ....]]
+# EN: possible values of each attribute	[name, [value1, value2, ....]]
+# ES: valores a tomar de cada atributo 	[atributo, [valor1, valor2, ....]]
 attributes = [
 	["tiporoca", 		["arcilla", "niquel", "hierro", "desconocido"]	],
 	["diametro", 		[ 1, 2, 3, 4, 5] 								],
@@ -17,37 +25,36 @@ attributes = [
 	["class", 			["Coronis", "Eos", "Temis", "Flora", "Maria"]	]
 ]
 
-[
-	['tiporoca', ['arcilla', 'niquel', 'hierro', 'desconocido']], 
-	['diametro', [1, 2, 3, 4, 5]], 
-	['masa', [2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3]], 
-	['forma', ['dentada', 'irregular']], 
-	['riezgoimpacto', ['bajo', 'medio', 'alto']], 
-	['class', ['Coronis', 'Eos', 'Temis', 'Flora', 'Maria']]
-]
 
 
-
-
-
+# EN: write the .arff file: header, @attribute declarations, then @data rows
+# ES: genera el archivo .arff: cabecera, declaraciones @attribute y filas @data
 with open (file=relacion+".arff" , mode="w+") as file:
+	# EN: @relation header 
+	# ES: cabecera @relation
 	file.write(f"@relation {relacion}\n\n")
-	
+
+	# EN: one @attribute line per attribute, values inside { } 
+	# ES: una línea @attribute por atributo, valores entre { }
 	for i in range(len(attributes)):
 		file.write(f"@attribute {attributes[i][0]} " + "{")
 		for j in range(len(attributes[i][1])):
 			file.write(f"{str(attributes[i][1][j])}, ")
 		file.seek(file.tell()-2)
-		file.write("}\n")	
+		file.write("}\n")
 
+	# EN: @data section, one random row per iteration 
+	# ES: sección @data, una fila aleatoria por iteración
 	file.write(f"\n@data\n")
 	for z in range(datas):
 		for i in range(len(attributes)):
 			rand = random.randint(0, len(attributes[i][1])-1)
 			value = attributes[i][1][rand]
+			# EN: quote numbers so Weka treats them as nominal 
+			# ES: entrecomilla números para que Weka los trate como nominales
 			if type(value) == int or type(value) == float:
 				value = "'" + str(value) + "'"
-			
+
 			file.write(f"{value}, ")
 		file.seek(file.tell()-2)
 		file.write("\n")
